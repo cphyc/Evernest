db7f48e104856d4438e0daced7c4615ded8fbae3
R1kM <aymeric.fr@hotmail.fr>
2014-10-04 02:08:41 +0200

    On essaie

---
+++ SimulBackend.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace Application
+{
+	public class Event // déclare t'on la class publique ?
+	{
+		public Event (int id, string content)
+		{
+			Date = DateTime.Now;
+			Id = id;
+			Content = content;
+		}
+		public Event (): this(0, "") {}
+
+		public DateTime Date { get; set; }
+		public int Id { get; set; }
+		public string Content { get; set; }
+	}
+	public class SimulBackend
+	{
+		// On implémente : get (n), put (event), get (n1, n2), rand() 
+		// On suppose que le string renvoyé est codé en JSON
+		public Event Get(int n) { return new Event ();}
+		public Event[] Get(int n1, int n2) {
+			Event[] ret = new Event[n2-n1];
+			return ret;
+		}
+		public Event rand() {return new Event(); }
+		public void Put (Event e) {}
+	}
+}
+
